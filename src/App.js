df66552e299a1c65e88345999cbfb26f440f0142
minhong1126 <83833271+minhong1126@users.noreply.github.com>
2024-03-12 20:00:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,39 +32,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import { useState, useEffect } from 'react';
-// import './App.css';
-// import useStore from './state.js'
-
-// function App() {
-//   const { loading, setLoading, coins, setCoins} = useStore(state => state);
-//   useEffect(() => {
-//     fetch("https://api.coinpaprika.com/v1/tickers")
-//     .then((res) => {
-//       res.json()
-//       console.log(res.json())
-//     })
-//     .then((json) => {
-//       setCoins(json);
-//       setLoading(false);
-//     });
-//   }, []);
-
-//   return (
-//     <>
-//     <h1> Coins </h1>
-//     {loading? (<strong> loading </strong>) : (
-//       <ul>
-//         {coins.map((coin) => {
-//           <li> {coin.name} : {coin.symbol}</li>
-//         })}
-//       </ul>
-//     )}
-//     </>
-//   );
-// }
-
-// export default App;
